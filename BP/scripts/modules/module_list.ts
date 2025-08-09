4f666c15d0f9aab9dd5271d17ec162dad6075692
phosxd <phosphorusxd@proton.me>
2025-08-09 17:39:03 -0400

    Remove duplicate files.

--- BP/scripts/modules/module_list.ts
+++
@@ -1,7 +0,0 @@
-export {List, ShardModule, ShardEvent, ShardCommand, ShardForm};
-import ShardModule from '../ShardAPI/module';
-import ShardEvent from '../ShardAPI/event';
-import ShardCommand from '../ShardAPI/command';
-import ShardForm from '../ShardAPI/form';
-
-const List:Array<ShardModule> = [];
